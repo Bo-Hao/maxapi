b548e8c56a340a55ebec8d240d36704559bae2a9
BoHao <bohaoperngbh@gmail.com>
2021-11-05 17:48:22 +0800

    20211105

---
+++ max.go
@@ -0,0 +1,593 @@
+package maxapi
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"log"
+	"strconv"
+	"sync"
+	"time"
+
+	"github.com/gorilla/websocket"
+	"github.com/shopspring/decimal"
+)
+
+func (Mc *MaxClient) Run() {
+	//var wg sync.WaitGroup
+	go func() {
+		Mc.SubscribeWS()
+	}()
+
+	go func() {
+		for {
+			select {
+			case <-Mc.ctx.Done():
+				fmt.Println("Done")
+				return
+			default:
+				time.Sleep(5 * time.Second)
+				fmt.Println("Local Balance", Mc.LocalBalance)
+				fmt.Println("Local Orders", Mc.LimitOrders)
+				Mc.cancelFunc()
+			}
+
+		}
+	}()
+}
+
+func (Mc *MaxClient) RoutineChecking() {
+	go func() {
+		fmt.Println("6 second to cancel")
+		time.Sleep(6 * time.Second)
+		Mc.cancelFunc()
+	}()
+
+}
+
+func NewMaxClient(APIKEY, APISECRET string) MaxClient {
+	ctx, cancel := context.WithCancel(context.Background())
+
+	// api client
+	cfg := NewConfiguration()
+	apiclient := NewAPIClient(cfg)
+
+	// Get markets []Market
+	markets, _, err := apiclient.PublicApi.GetApiV2Markets(ctx)
+	if err != nil {
+		log.Print(err)
+	}
+
+	return MaxClient{
+		apiKey:    APIKEY,
+		apiSecret: APISECRET,
+
+		ctx:        ctx,
+		cancelFunc: cancel,
+
+		ApiClient: apiclient,
+
+		LimitOrders: map[int32]WsOrder{},
+		DoneOrders:  map[int32]WsOrder{},
+
+		Markets:      markets,
+		LocalBalance: map[string]float64{},
+		LocalLocked:  map[string]float64{},
+	}
+}
+
+type MaxClient struct {
+	apiKey    string
+	apiSecret string
+
+	ctx        context.Context
+	cancelFunc context.CancelFunc
+
+	// web socket client
+	WsClient WebsocketClient
+	// api client
+	ApiClient *APIClient
+
+	// local orders record
+	LimitOrders      map[int32]WsOrder
+	LimitOrdersMutex sync.RWMutex
+	DoneOrders       map[int32]WsOrder
+	DoneOrdersMutex  sync.RWMutex
+
+	// All markets pairs
+	Markets []Market
+
+	// Account
+	Account Member
+
+	// local balance
+	LocalBalance      map[string]float64 // currency balance
+	LocalBalanceMutex sync.RWMutex
+	LocalLocked       map[string]float64 // locked currency balance
+	LocalLockedMutex  sync.RWMutex
+}
+
+type WebsocketClient struct {
+	OnErr bool
+	Conn  *websocket.Conn
+
+	LastUpdatedId      decimal.Decimal
+	LastUpdatedIdMutex sync.RWMutex
+
+	TmpTrades      []Trade
+	TmpOrders      map[int32]WsOrder
+	TmpOrdersMutex sync.RWMutex
+}
+
+func (Mc *MaxClient) CancelAllOrders() ([]WsOrder, error) {
+	Mc.LimitOrdersMutex.Lock()
+	defer Mc.LimitOrdersMutex.Unlock()
+	canceledOrders, _, err := Mc.ApiClient.PrivateApi.PostApiV2OrdersClear(Mc.ctx, Mc.apiKey, Mc.apiSecret, nil)
+	if err != nil {
+		return []WsOrder{}, errors.New("fail to cancel all orders")
+	}
+	canceledWsOrders := make([]WsOrder, 0, len(canceledOrders))
+
+	log.Printf("Cancel %d Orders.", len(canceledOrders))
+
+	// data update
+	// local balance update
+	for i := 0; i < len(canceledOrders); i++ {
+		canceledWsOrders = append(canceledWsOrders, WsOrder(canceledOrders[i]))
+		order := canceledOrders[i]
+		side := order.Side
+		market := order.Market
+		price, err := strconv.ParseFloat(order.Price, 64)
+		if err != nil {
+			log.Print(err)
+		}
+		volume, err := strconv.ParseFloat(order.Volume, 64)
+		if err != nil {
+			log.Print(err)
+		}
+		Mc.updateLocalBalance(market, side, price, volume, true) // true means this is a cancel order.
+	}
+
+	return canceledWsOrders, nil
+}
+
+func (Mc *MaxClient) CancelOrder(market string, id int32) (WsOrder, error) {
+	CanceledOrder, _, err := Mc.ApiClient.PrivateApi.PostApiV2OrderDelete(Mc.ctx, Mc.apiKey, Mc.apiSecret, id)
+	if err != nil {
+		fmt.Println(err)
+		return WsOrder{}, errors.New("fail to cancel order" + strconv.Itoa(int(id)))
+	}
+
+	log.Printf("Cancel Order %v.", id)
+
+	// data update
+	// local balance update
+	order := CanceledOrder
+	side := order.Side
+	price, err := strconv.ParseFloat(order.Price, 64)
+	if err != nil {
+		log.Print(err)
+	}
+	volume, err := strconv.ParseFloat(order.Volume, 64)
+	if err != nil {
+		log.Print(err)
+	}
+
+	Mc.updateLocalBalance(market, side, price, volume, true) // true means this is a cancel order
+
+	return WsOrder(CanceledOrder), nil
+}
+
+/*	"side" (string) set tp cancel only sell (asks) or buy (bids) orders
+	"market" (string) specify market like btctwd / ethbtc
+	both params can be set as nil.
+*/
+func (Mc *MaxClient) CancelOrders(market, side interface{}) ([]WsOrder, error) {
+	params := make(map[string]interface{})
+	if market != nil {
+		params["market"] = market.(string)
+	}
+	if side != nil {
+		params["side"] = side.(string)
+	}
+
+	canceledOrders, _, err := Mc.ApiClient.PrivateApi.PostApiV2OrdersClear(Mc.ctx, Mc.apiKey, Mc.apiSecret, params)
+	if err != nil {
+		fmt.Println(err)
+		return []WsOrder{}, errors.New("fail to cancel orders")
+	}
+	canceledWsOrders := make([]WsOrder, 0, len(canceledOrders))
+
+	log.Printf("Cancel %d Orders.", len(canceledOrders))
+
+	// local balance update
+	for i := 0; i < len(canceledOrders); i++ {
+		// update local orders
+		wsOrder := WsOrder(canceledOrders[i])
+		canceledWsOrders = append(canceledWsOrders, wsOrder)
+		order := canceledOrders[i]
+		side := order.Side
+		market := order.Market
+		price, err := strconv.ParseFloat(order.Price, 64)
+		if err != nil {
+			log.Print(err)
+		}
+		volume, err := strconv.ParseFloat(order.Volume, 64)
+		if err != nil {
+			log.Print(err)
+		}
+		Mc.updateLocalBalance(market, side, price, volume, true) // true means this is a cancel order.
+	}
+
+	return canceledWsOrders, nil
+}
+
+func (Mc *MaxClient) PlaceLimitOrder(market string, side string, price, volume float64) (WsOrder, error) {
+	if isEnough := Mc.checkBalanceEnoughLocal(market, side, price, volume); !isEnough {
+		return WsOrder{}, errors.New("balance is not enough for trading")
+	}
+
+	params := make(map[string]interface{})
+	params["price"] = fmt.Sprint(price)
+	params["ordType"] = "limit"
+	vol := fmt.Sprint(volume)
+
+	order, _, err := Mc.ApiClient.PrivateApi.PostApiV2Orders(Mc.ctx, Mc.apiKey, Mc.apiSecret, market, side, vol, params)
+	if err != nil {
+		return WsOrder{}, errors.New("fail to place limit order")
+	}
+
+	// data update
+	// local balance update
+	Mc.updateLocalBalance(market, side, price, volume, false) // false means this is a normal order
+	return WsOrder(order), nil
+}
+
+// temporarily cannot work
+func (Mc *MaxClient) PlaceMultiLimitOrders(market string, sides []string, prices, volumes []float64) ([]WsOrder, error) {
+	// check not zero
+	if len(sides) == 0 || len(prices) == 0 || len(volumes) == 0 {
+		return []WsOrder{}, errors.New("fail to construct multi limit orders")
+	}
+
+	// check length
+	if len(sides) != len(prices) || len(prices) != len(volumes) {
+		return []WsOrder{}, errors.New("fail to construct multi limit orders")
+	}
+
+	optionalMap := map[string]interface{}{}
+	ordersPrice := make([]string, 0, len(prices))
+	ordersVolume := make([]string, 0, len(prices))
+
+	totalVolumeBuy, totalVolumeSell := 0., 0.
+	weightedPriceBuy, weightedPriceSell := 0., 0.
+	countSideBuy, countSideSell := 0, 0
+	for i := 0; i < len(prices); i++ {
+		ordersPrice = append(ordersPrice, fmt.Sprintf("%g", prices[i]))
+		ordersVolume = append(ordersVolume, fmt.Sprintf("%g", volumes[i]))
+
+		switch sides[i] {
+		case "buy":
+			totalVolumeBuy += volumes[i]
+			weightedPriceBuy += prices[i] * volumes[i]
+			countSideBuy++
+		case "sell":
+			totalVolumeSell += volumes[i]
+			weightedPriceSell += prices[i] * volumes[i]
+			countSideSell++
+		}
+	}
+
+	// check if the balance enough or not.
+	buyEnough, sellEnough := false, false
+	if totalVolumeBuy == 0. {
+		buyEnough = true
+	} else {
+		weightedPriceBuy /= totalVolumeBuy
+		buyEnough = Mc.checkBalanceEnoughLocal(market, "buy", weightedPriceBuy, totalVolumeBuy)
+	}
+
+	if totalVolumeSell == 0. {
+		sellEnough = true
+	} else {
+		weightedPriceSell /= totalVolumeSell
+		sellEnough = Mc.checkBalanceEnoughLocal(market, "sell", weightedPriceSell, totalVolumeSell)
+	}
+
+	// if there is one side lack of balance.
+	if !buyEnough || !sellEnough {
+		return []WsOrder{}, errors.New("there is no enough balance for placing multi orders")
+	}
+
+	optionalMap["orders[price]"] = ordersPrice
+
+	// main api function
+	orders, _, err := Mc.ApiClient.PrivateApi.PostApiV2OrdersMulti(Mc.ctx, Mc.apiKey, Mc.apiSecret, market, sides, ordersVolume, optionalMap)
+	if err != nil {
+		fmt.Println(err)
+		return []WsOrder{}, errors.New("fail to place multi-limit orders")
+	}
+
+	// data update
+	// local balance update
+	Mc.updateLocalBalance(market, "buy", weightedPriceBuy, totalVolumeBuy, false)    // false means this is a normal order
+	Mc.updateLocalBalance(market, "sell", weightedPriceSell, totalVolumeSell, false) // false means this is a normal order
+
+	wsOrders := make([]WsOrder, 0, len(orders))
+	for i := 0; i < len(orders); i++ {
+		wsOrders = append(wsOrders, WsOrder(orders[i]))
+	}
+	return wsOrders, nil
+}
+
+func (Mc *MaxClient) PlaceMarketOrder(market string, side string, volume float64) (WsOrder, error) {
+	params := make(map[string]interface{})
+	params["ordType"] = "market"
+	vol := fmt.Sprint(volume)
+	order, _, err := Mc.ApiClient.PrivateApi.PostApiV2Orders(Mc.ctx, Mc.apiKey, Mc.apiSecret, market, side, vol, params)
+	if err != nil {
+		return WsOrder{}, errors.New("fail to place market orders")
+	}
+
+	// local balance update
+	price, err := strconv.ParseFloat(order.Price, 64)
+	if err != nil {
+		log.Print(err)
+	}
+	Mc.updateLocalBalance(market, side, price, volume, false) // false means this is a normal order
+
+	return WsOrder(order), nil
+}
+
+func (Mc *MaxClient) GetAccount() (Member, error) {
+	member, _, err := Mc.ApiClient.PrivateApi.GetApiV2MembersAccounts(Mc.ctx, Mc.apiKey, Mc.apiSecret)
+	if err != nil {
+		fmt.Println(err)
+		return Member{}, errors.New("fail to get account")
+	}
+
+	Mc.Account = member
+	return member, nil
+}
+
+func (Mc *MaxClient) GetBalance() ([]Account, error) {
+	member, _, err := Mc.ApiClient.PrivateApi.GetApiV2MembersAccounts(Mc.ctx, Mc.apiKey, Mc.apiSecret)
+	if err != nil {
+		fmt.Println(err)
+		return []Account{}, errors.New("fail to get balance")
+	}
+	return member.Accounts, nil
+}
+
+// GET account and sent the balance to the local balance
+func (Mc *MaxClient) BalanceGlobal2Local() error {
+	Member, err := Mc.GetAccount()
+	if err != nil {
+		log.Print(err)
+	}
+
+	Mc.LocalBalanceMutex.Lock()
+	defer Mc.LocalBalanceMutex.Unlock()
+
+	Accounts := Member.Accounts
+	for i := 0; i < len(Accounts); i++ {
+		currency := Accounts[i].Currency
+		balance, err := strconv.ParseFloat(Accounts[i].Balance, 64)
+		if err != nil {
+			log.Print(err)
+			return errors.New("fail to parse balance to float64")
+		}
+		locked, err := strconv.ParseFloat(Accounts[i].Locked, 64)
+		if err != nil {
+			log.Print(err)
+			return errors.New("fail to parse locked balance to float64")
+		}
+
+		Mc.LocalBalance[currency] = balance
+		Mc.LocalLocked[currency] = locked
+	}
+
+	return nil
+}
+
+func (Mc *MaxClient) GetOrders(market string) ([]WsOrder, error) {
+	orders, _, err := Mc.ApiClient.PrivateApi.GetApiV2Orders(Mc.ctx, Mc.apiKey, Mc.apiSecret, market, nil)
+	if err != nil {
+		fmt.Println(err)
+		return []WsOrder{}, errors.New("fail to get order list")
+	}
+
+	wsOrders := make([]WsOrder, 0, len(orders))
+	for i := 0; i < len(orders); i++ {
+		wsOrders = append(wsOrders, WsOrder(orders[i]))
+	}
+
+	return wsOrders, nil
+}
+
+func (Mc *MaxClient) OrderGlobal2Local() error {
+	newOrders := map[int32]WsOrder{}
+	markets, _, err := Mc.ApiClient.PublicApi.GetApiV2Markets(Mc.ctx)
+	if err != nil {
+		return errors.New("fail to get market")
+	}
+
+	for i := 0; i < len(markets); i++ {
+		marketId := markets[i].Id
+		wsOrders, err := Mc.GetOrders(marketId)
+		if err != nil {
+			errMsg := fmt.Sprintf("fail tp get %s orders", marketId)
+			return errors.New(errMsg)
+		}
+		for j := 0; j < len(wsOrders); j++ {
+			newOrders[wsOrders[j].Id] = wsOrders[j]
+		}
+	}
+
+	Mc.LimitOrdersMutex.Lock()
+	defer Mc.LimitOrdersMutex.Unlock()
+	Mc.LimitOrders = newOrders
+	return nil
+}
+
+func (Mc *MaxClient) DetectDoneOrders() map[string]HedgingOrder {
+	hedgingOrders := map[string]HedgingOrder{}
+	if len(Mc.DoneOrders) == 0 {
+		return hedgingOrders
+	}
+
+	Mc.DoneOrdersMutex.Lock()
+	defer Mc.DoneOrdersMutex.Unlock()
+
+	for _, order := range Mc.DoneOrders {
+		market := order.Market
+		price, err := strconv.ParseFloat(order.Price, 64)
+		if err != nil {
+			log.Print(err)
+		}
+		volume, err := strconv.ParseFloat(order.Volume, 64)
+		if err != nil {
+			log.Print(err)
+		}
+
+		s := 1.
+		if order.Side == "buy" || order.Side == "bid" {
+			s = -1.
+		}
+
+		timestamp := int32(time.Now().UnixMilli())
+
+		if hegingOrder, ok := hedgingOrders[market]; ok {
+			hegingOrder.PartialProfit += price * volume * s
+			hegingOrder.TotalVolume += volume * s
+		} else {
+			ho := HedgingOrder{
+				Market:        market,
+				PartialProfit: price * volume * s,
+				TotalVolume:   volume * s,
+				Timestamp:     timestamp,
+			}
+			hedgingOrders[market] = ho
+		}
+	}
+
+	for _, hedgingOrder := range hedgingOrders {
+		if hedgingOrder.TotalVolume != 0 {
+			hedgingOrder.State = "wait"
+			if hedgingOrder.TotalVolume > 0 {
+				hedgingOrder.HedgeSide = "sell"
+				hedgingOrder.HedgeVolume = hedgingOrder.TotalVolume
+			} else {
+				hedgingOrder.HedgeSide = "buy"
+				hedgingOrder.HedgeVolume = hedgingOrder.TotalVolume * -1
+			}
+		} else {
+			hedgingOrder.State = "done"
+			hedgingOrder.TotalProfit = hedgingOrder.PartialProfit
+		}
+	}
+
+	Mc.DoneOrders = map[int32]WsOrder{}
+	return hedgingOrders
+}
+
+type HedgingOrder struct {
+	// order
+	Market        string
+	PartialProfit float64
+	TotalVolume   float64
+	Timestamp     int32
+
+	// hedging state
+	State       string
+	TotalProfit float64
+
+	// the order which should be sent
+	HedgeVolume float64
+	HedgeSide   string
+}
+
+// ########### assistant functions ###########
+
+func (Mc *MaxClient) checkBaseQuote(market string) (base, quote string, err error) {
+	markets := Mc.Markets
+	for _, m := range markets {
+		if m.Id == market {
+			base = m.BaseUnit
+			quote = m.QuoteUnit
+			err = nil
+			return
+		}
+	}
+	err = errors.New("market not exist")
+	return
+}
+
+// volume is the volume of base currency. return true denote enough for trading.
+func (Mc *MaxClient) checkBalanceEnoughLocal(market, side string, price, volume float64) (enough bool) {
+	Mc.LocalBalanceMutex.RLock()
+	defer Mc.LocalBalanceMutex.RUnlock()
+
+	base, quote, err := Mc.checkBaseQuote(market)
+	if err != nil {
+		log.Print(err)
+		return false
+	}
+	switch side {
+	case "sell":
+		baseBalance := Mc.LocalBalance[base]
+		if baseBalance > volume {
+			enough = true
+		}
+	case "buy":
+		needed := price * volume
+		quoteBalance := Mc.LocalBalance[quote]
+		if quoteBalance >= needed {
+			enough = true
+		}
+	} // end switch
+	return
+}
+
+func (Mc *MaxClient) updateLocalBalance(market, side string, price, volume float64, gain bool) error {
+	Mc.LocalBalanceMutex.Lock()
+	defer Mc.LocalBalanceMutex.Unlock()
+	base, quote, err := Mc.checkBaseQuote(market)
+	if err != nil {
+		log.Print(err)
+		return errors.New("fail to update local balance")
+	}
+	switch side {
+	case "sell":
+		if gain {
+			Mc.LocalBalance[base] += volume
+			Mc.LocalLocked[base] -= volume
+		} else {
+			Mc.LocalBalance[base] -= volume
+			Mc.LocalLocked[base] += volume
+
+		}
+	case "buy":
+		needed := price * volume
+		if gain {
+			Mc.LocalBalance[quote] += needed
+			Mc.LocalLocked[quote] -= needed
+		} else {
+			Mc.LocalBalance[quote] -= needed
+			Mc.LocalLocked[quote] += needed
+		}
+	}
+	return nil
+}
+
+func SendHedgingOrders(hedgingOrders map[string]HedgingOrder) error {
+	if len(hedgingOrders) == 0 {
+		return nil
+	}
+
+	for key, val := range hedgingOrders {
+		fmt.Println(key, val)
+	}
+
+	return nil
+}
---
+++ max_websocket.go
@@ -0,0 +1,440 @@
+package maxapi
+
+import (
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"reflect"
+	"strconv"
+	"time"
+
+	"github.com/gorilla/websocket"
+)
+
+func (Mc *MaxClient) SubscribeWS() {
+	var url string = "wss://max-stream.maicoin.com/ws"
+	conn, _, err := websocket.DefaultDialer.Dial(url, nil)
+	if err != nil {
+		log.Print(err)
+	}
+	log.Println("Connected:", url)
+
+	subMsg, err := GetMaxSubscribePrivateMessage(Mc.apiKey, Mc.apiSecret)
+	if err != nil {
+		log.Print(errors.New("fail to construct subscribtion message"))
+	}
+
+	err = conn.WriteMessage(websocket.TextMessage, subMsg)
+	if err != nil {
+		log.Print(errors.New("fail to subscribe websocket"))
+	}
+	Mc.WsClient.Conn = conn
+	Mc.WsClient.OnErr = false
+	defer conn.Close()
+
+	// mainloop
+	for {
+		select {
+		case <-Mc.ctx.Done():
+			fmt.Println("stop")
+			Mc.CancelAllOrders()
+			Mc.WsClient.Conn.Close()
+			return
+		default:
+			if Mc.WsClient.Conn == nil {
+				Mc.WsClient.OnErr = true
+				message := "max websocket reconnecting"
+				log.Print(message)
+			}
+
+			_, msg, err := conn.ReadMessage()
+			if err != nil {
+				log.Println("read:", err)
+				Mc.WsClient.OnErr = true
+				message := "max websocket reconnecting"
+				log.Print(message)
+			}
+
+			var msgMap map[string]interface{}
+			err = json.Unmarshal(msg, &msgMap)
+			if err != nil {
+				log.Print(err)
+				Mc.WsClient.OnErr = true
+			}
+
+			errh := Mc.handleMAXSocketMsg(msg)
+			if errh != nil {
+				Mc.WsClient.OnErr = true
+				message := "max websocket reconnecting"
+				log.Print(message)
+			}
+			time.Sleep(1 * time.Second)
+		} // end select
+
+		// if there is something wrong that the WS should be reconnected.
+		if Mc.WsClient.OnErr {
+			break
+		}
+	} // end for
+
+	Mc.WsClient.Conn.Close()
+	// if it is manual work.
+	if Mc.WsClient.OnErr {
+		Mc.WsClient.TmpOrdersMutex.Lock()
+		Mc.WsClient.TmpOrders = Mc.LimitOrders
+		Mc.WsClient.TmpOrdersMutex.Unlock()
+		Mc.SubscribeWS()
+	}
+}
+
+func GetMaxSubscribeMessage(product, channel string, symbols []string) ([]byte, error) {
+	param := make(map[string]interface{})
+	param["action"] = "sub"
+
+	var args []map[string]interface{}
+	for _, symbol := range symbols {
+		subscriptions := make(map[string]interface{})
+		subscriptions["channel"] = channel
+		subscriptions["market"] = symbol
+		subscriptions["depth"] = 1
+		args = append(args, subscriptions)
+	}
+
+	param["subscriptions"] = args
+	req, err := json.Marshal(param)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+// provide private subscribtion message.
+func GetMaxSubscribePrivateMessage(apikey, apisecret string) ([]byte, error) {
+	// making signature
+	h := hmac.New(sha256.New, []byte(apisecret))
+	nonce := time.Now().UnixMilli()               // millisecond.
+	h.Write([]byte(strconv.FormatInt(nonce, 10))) // int64 to string.
+	signature := hex.EncodeToString(h.Sum(nil))
+
+	// prepare authentication message.
+	param := make(map[string]interface{})
+	param["action"] = "auth"
+	param["apiKey"] = apikey
+	param["nonce"] = nonce
+	param["signature"] = signature
+	//param["filters"] = []string{filter}
+	param["id"] = "User"
+
+	req, err := json.Marshal(param)
+	if err != nil {
+		return nil, err
+	}
+	return req, nil
+}
+
+// ##### #####
+
+func (Mc *MaxClient) handleMAXSocketMsg(msg []byte) error {
+	var msgMap map[string]interface{}
+	err := json.Unmarshal(msg, &msgMap)
+	if err != nil {
+		log.Print(err)
+		return errors.New("fail to unmarshal message")
+	}
+
+	event, ok := msgMap["e"]
+	if !ok {
+		log.Print("there is no event in message")
+		return errors.New("fail to obtain message")
+	}
+
+	// distribute the msg
+	var err2 error
+	switch event {
+	case "authenticated":
+		log.Print("websocket subscribtion authenticated")
+	case "order_snapshot":
+		err2 = Mc.parseOrderSnapshotMsg(msgMap)
+	case "trade_snapshot":
+		err2 = Mc.parseTradeSnapshotMsg(msgMap)
+	case "account_snapshot":
+		err2 = Mc.parseAccountMsg(msgMap)
+	case "order_update":
+		err2 = Mc.parseOrderUpdateMsg(msgMap)
+	case "trade_update":
+		err2 = Mc.parseTradeUpdateMsg(msgMap)
+	case "account_update":
+		err2 = Mc.parseAccountMsg(msgMap)
+	}
+	if err2 != nil {
+		return errors.New("fail to parse message")
+	}
+	return nil
+}
+
+// Order
+//	order_snapshot
+func (Mc *MaxClient) parseOrderSnapshotMsg(msgMap map[string]interface{}) error {
+	snapshotWsOrders := map[int32]WsOrder{}
+	jsonbody, _ := json.Marshal(msgMap["o"])
+	var wsOrders []WsOrder
+	json.Unmarshal(jsonbody, &wsOrders)
+
+	for i := 0; i < len(wsOrders); i++ {
+		snapshotWsOrders[wsOrders[i].Id] = wsOrders[i]
+	}
+
+	// checking trades situation.
+	err := Mc.trackingOrders(snapshotWsOrders)
+	if err != nil {
+		log.Print("fail to check the trades during disconnection")
+	}
+
+	Mc.trackingOrders(snapshotWsOrders)
+	return nil
+}
+
+//	order_update
+func (Mc *MaxClient) parseOrderUpdateMsg(msgMap map[string]interface{}) error {
+	jsonbody, _ := json.Marshal(msgMap["o"])
+	var wsOrders []WsOrder
+	json.Unmarshal(jsonbody, &wsOrders)
+
+	Mc.LimitOrdersMutex.Lock()
+	defer Mc.LimitOrdersMutex.Unlock()
+	Mc.DoneOrdersMutex.Lock()
+	defer Mc.DoneOrdersMutex.Unlock()
+
+	for i := 0; i < len(wsOrders); i++ {
+		if _, ok := Mc.LimitOrders[wsOrders[i].Id]; !ok {
+			Mc.LimitOrders[wsOrders[i].Id] = wsOrders[i]
+		} else {
+			switch wsOrders[i].State {
+			case "cancel":
+				delete(Mc.LimitOrders, wsOrders[i].Id)
+			case "done":
+				Mc.DoneOrders[wsOrders[i].Id] = wsOrders[i]
+				delete(Mc.LimitOrders, wsOrders[i].Id)
+			default:
+				fmt.Println(wsOrders[i], " waiting...")
+				if _, ok := Mc.LimitOrders[wsOrders[i].Id]; !ok {
+					Mc.LimitOrders[wsOrders[i].Id] = wsOrders[i]
+				}
+			}
+		}
+	}
+	return nil
+}
+
+// Trade
+//	trade_snapshot
+func (Mc *MaxClient) parseTradeSnapshotMsg(msgMap map[string]interface{}) error {
+	snapshotTrades := make([]Trade, 0, 100)
+	switch reflect.TypeOf(msgMap["t"]).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(msgMap["t"])
+		for i := 0; i < s.Len(); i++ {
+			wsTrade := s.Index(i).Interface().(map[string]interface{})
+
+			id, err := strconv.Atoi(wsTrade["i"].(string))
+			if err != nil {
+				return errors.New("fail to convert id from string to int")
+			}
+
+			T, err := strconv.Atoi(wsTrade["T"].(string))
+			if err != nil {
+				return errors.New("fail to convert T from string to int")
+			}
+
+			side, err := sellbuyTransfer(wsTrade["sd"].(string))
+			if err != nil {
+				return err
+			}
+			maker := true
+			if wsTrade["m"] == "false" {
+				maker = false
+			}
+
+			newTrade := Trade{
+				Id:          int32(id),
+				Price:       wsTrade["p"].(string),
+				Volume:      wsTrade["v"].(string),
+				Market:      wsTrade["M"].(string),
+				Timestamp:   int32(T),
+				Side:        side,
+				Fee:         wsTrade["f"].(string),
+				FeeCurrency: wsTrade["fc"].(string),
+				Maker:       maker,
+			}
+			snapshotTrades = append(snapshotTrades, newTrade)
+		} // end for
+	} // end switch
+
+	// checking trades situation.
+	/* err := Mc.trackingTrades(snapshotTrades)
+	if err != nil {
+		log.Print("fail to check the trades during disconnection")
+	} */
+
+	return nil
+}
+
+func (Mc *MaxClient) trackingOrders(snapshotWsOrders map[int32]WsOrder) error {
+	Mc.LimitOrdersMutex.Lock()
+	defer Mc.LimitOrdersMutex.Unlock()
+	Mc.WsClient.TmpOrdersMutex.Lock()
+	defer Mc.WsClient.TmpOrdersMutex.Unlock()
+
+	// if there is not orders in the tmp memory, it is not possible to track the trades during WS is disconnected.
+	if len(Mc.WsClient.TmpOrders) == 0 {
+		Mc.LimitOrders = snapshotWsOrders
+		return nil
+	}
+
+	untrackedWsOrders := map[int32]WsOrder{}
+	trackedWsOrders := map[int32]WsOrder{}
+
+	for wsorderId, wsorder := range Mc.WsClient.TmpOrders {
+		if _, ok := snapshotWsOrders[wsorderId]; ok && wsorder.State != "Done" {
+			trackedWsOrders[wsorderId] = wsorder
+		} else {
+			untrackedWsOrders[wsorderId] = wsorder
+		}
+	}
+
+	Mc.LimitOrders = trackedWsOrders
+
+	Mc.DoneOrdersMutex.Lock()
+	defer Mc.DoneOrdersMutex.Unlock()
+	for id, odr := range untrackedWsOrders {
+		if _, ok := Mc.DoneOrders[id]; !ok {
+			Mc.DoneOrders[id] = odr
+		}
+	}
+
+	return nil
+}
+
+//	trade_update
+func (Mc *MaxClient) parseTradeUpdateMsg(msgMap map[string]interface{}) error {
+	var newTrades []Trade
+	switch reflect.TypeOf(msgMap["t"]).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(msgMap["t"])
+		for i := 0; i < s.Len(); i++ {
+			wsTrade := s.Index(i).Interface().(map[string]interface{})
+			id, err := strconv.Atoi(wsTrade["i"].(string))
+			if err != nil {
+				return errors.New("fail to convert id from string to int")
+			}
+
+			T, err := strconv.Atoi(wsTrade["T"].(string))
+			if err != nil {
+				return errors.New("fail to convert T from string to int")
+			}
+
+			side, err := sellbuyTransfer(wsTrade["sd"].(string))
+			if err != nil {
+				return err
+			}
+			maker := true
+			if wsTrade["m"] == "false" {
+				maker = false
+			}
+
+			newTrade := Trade{
+				Id:          int32(id),
+				Price:       wsTrade["p"].(string),
+				Volume:      wsTrade["v"].(string),
+				Market:      wsTrade["M"].(string),
+				Timestamp:   int32(T),
+				Side:        side,
+				Fee:         wsTrade["f"].(string),
+				FeeCurrency: wsTrade["fc"].(string),
+				Maker:       maker,
+			}
+			newTrades = append(newTrades, newTrade)
+		} // end for
+	} // end switch
+
+	//Mc.UnhedgedTrades = append(Mc.UnhedgedTrades, newTrades...)
+	return nil
+}
+
+type Trade struct {
+	Id          int32
+	Price       string
+	Volume      string
+	Market      string
+	Timestamp   int32
+	Side        string
+	Fee         string
+	FeeCurrency string
+	Maker       bool
+}
+
+// Account
+//	account_snapshot and //	account_update
+func (Mc *MaxClient) parseAccountMsg(msgMap map[string]interface{}) error {
+	Mc.LocalBalanceMutex.Lock()
+	defer Mc.LocalBalanceMutex.Unlock()
+	switch reflect.TypeOf(msgMap["B"]).Kind() {
+	case reflect.Slice:
+		s := reflect.ValueOf(msgMap["B"])
+		for i := 0; i < s.Len(); i++ {
+			wsCurrency := s.Index(i).Interface().(map[string]interface{})
+			wsBalance, err := strconv.ParseFloat(wsCurrency["av"].(string), 64)
+			if err != nil {
+				wsBalance = 0.0
+				return errors.New("fail to parse float")
+
+			}
+
+			wsLocked, err := strconv.ParseFloat(wsCurrency["l"].(string), 64)
+			if err != nil {
+				wsLocked = 0.0
+				return errors.New("fail to parse float")
+			}
+
+			Mc.LocalBalance[wsCurrency["cu"].(string)] = wsBalance
+			Mc.LocalLocked[wsCurrency["cu"].(string)] = wsLocked
+		} // end for
+	} // end switch
+
+	fmt.Println("WS bottom:", Mc.LocalBalance)
+	return nil
+}
+
+func sellbuyTransfer(side string) (string, error) {
+	switch side {
+	case "sell":
+		return "sell", nil
+	case "buy":
+		return "buy", nil
+	case "bid":
+		return "buy", nil
+	case "ask":
+		return "sell", nil
+	}
+	return "", errors.New("unrecognized side appear")
+}
+
+type WsOrder struct {
+	Id              int32  `json:"i,omitempty"`
+	Side            string `json:"sd,omitempty"`
+	OrdType         string `json:"ot,omitempty"`
+	Price           string `json:"p,omitempty"`
+	StopPrice       string `json:"sp,omitempty"`
+	AvgPrice        string `json:"ap,omitempty"`
+	State           string `json:"S,omitempty"`
+	Market          string `json:"M,omitempty"`
+	CreatedAt       int32  `json:"T,omitempty"`
+	Volume          string `json:"v,omitempty"`
+	RemainingVolume string `json:"rv,omitempty"`
+	ExecutedVolume  string `json:"ev,omitempty"`
+	TradesCount     int32  `json:"tc,omitempty"`
+}
